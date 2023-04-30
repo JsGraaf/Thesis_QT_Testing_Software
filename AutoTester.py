dd505ee02f913041163e7ae02f2e5bc6bbcf5820
s2573172 <s2573172@vuw.leidenuniv.nl>
2023-04-30 10:03:46 +0200

    Added output to csv code

--- AutoTester.py
+++ AutoTester.py
@@ -6,12 +6,18 @@ import argparse
 import subprocess
 import os
 import serial
+import xlsxwriter
+import datetime
 
 VERILOG_GENERATION_FILES_PATH = r"Verilog_Generators/"
 QORC_SDK_PATH = r"/home/joris/Uni/Scriptie/qorc_sdk/"
 QT_APP_DIR = r"QT_App"
 QT_APP_PATH = r"QT_App/fpga/rtl/"
 FPGA_FILE_PATH = os.path.join(QT_APP_PATH, "MODULE_top.v")
+OUTPUT_DIR = r"Output"
+OUTPUT_FILE = r"Power_Test_Output.xlsx"
+
+HEADERS = ["Voltage(V)", "Amperage(A)", "Wattage(mW)"]
 
 def parseArguments():
    argParser = argparse.ArgumentParser()
@@ -23,10 +29,10 @@ def parseArguments():
                           help="Specify the filename of the verilog generator, located in Verilog_Generators/")
    argParser.add_argument("-c", "--circuitCount", required=False,
                           help="Specify the amount of circuits to generate")
-   argParser.add_argument("-o", "--outFile", required=False,
-                          help="Path for the output file, must end in .csv")
    argParser.add_argument("-l", "--testLength", required=False,
                           help="Specify the length of the power tests")
+   argParser.add_argument("-d", "--delay", required=True, 
+                          help="Specify delay between measurements")
    return argParser.parse_args()
 
 def printError(errno: int):
@@ -86,19 +92,50 @@ def flashBoard(ser, QORC_PORT: str, ):
    if (val == 0): return True
    else: return False
 
+
+# Output measurements string to csv file
+# Sample seperated by ';', values by ':'
+def outputToCsv(sheetName: str, measurements: list):
+   # Check if output dir exists
+   if not os.path.isdir(OUTPUT_DIR):
+      os.mkdir(OUTPUT_DIR)
+   # Check if file is already present
+   if not os.path.isfile(os.path.join(OUTPUT_DIR, OUTPUT_FILE)):
+         workbook = xlsxwriter.Workbook(OUTPUT_FILE)
+   # Create new worksheet
+   while sheetName in workbook.sheetnames:
+      # Prevent overwriting
+      sheetName = "new_" + sheetName
+   worksheet = workbook.add_worksheet(sheetName)
+   # Add headers
+   for i in range(len(HEADERS)):
+      worksheet.write(0, i, HEADERS[i])
+   # Add date as final header
+   worksheet.write(0, 3, datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+   # Add data
+   rowCounter = 1 # Start at one due to header row
+   # Split measurments string
+   measurements = measurements.split(';')
+   for row in measurements:
+      sample = row.split(':')
+      for column in range(len(sample)):
+         worksheet.write(rowCounter, column, sample[column])
+   
+   workbook.close()
+   return True
 # Run power tests by sending serial command to uC connected to INA219 to 
 # perform power tests for the desired amount of time
 # comPort: Port to which the uC with the INA219 is connected
-def runPowerTests(ser, testLength: int, betweenTime: int, outFile: os.path):
+def runPowerTests(ser, testLength: int, betweenTime: int, outName: str):
    # Send command to uC to confirm link
    ser.write("T")
    if (ser.read() != 'C'): return False
-   # Send command to uC to start testing for power
+   # Send command to uC to start testing for power with length and sample delay
    ser.write("{} {}".format(testLength, betweenTime))
-   # Send command with total sampling time in ms
-   # Send command with time in between samples in ms
    # Receive all the data and chop up into individual measurements
+   measurements = ser.read_until('\n')
    # Output to CSV file
+   outputToCsv(outName, measurements)
    return True
 
 if __name__ == '__main__':
